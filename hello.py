11e0a169305a69edd4e72cf649f58ad1620c97c3
Zach Liou <zachliou@gmail.com>
2023-08-09 21:28:20 -0700

    Add basic second route.

--- hello.py
+++ hello.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p><a href='/there'>let's go</a>"
 
+
+
+@app.route("/there")
+def hello_there():
+    return "<p>hello there!</p>"
